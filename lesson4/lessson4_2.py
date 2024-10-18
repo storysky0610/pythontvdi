645a35f82501630101f0c04403db02582fd4addc
13 <st27788711@gmail.com>
2024-10-18 11:33:42 +0800

    lesson4_2_3

--- lesson4/lessson4_2.py
+++ lesson4/lessson4_2.py
@@ -1,8 +1,13 @@
 import tkinter as tk
-root = tk.Tk()
-print(type(root))
-root.title('這是我的第一個視窗')
-root.geometry('800x300')
-message = tk.Label(root,text="Hollo!這是我的第一個視窗")
-message.pack()
-root.mainloop()
+def main():
+    root = tk.Tk()
+    print(type(root))
+    root.title('這是我的第一個視窗')
+    root.geometry('800x300')
+    message = tk.Label(root,text="Hollo!這是我的第一個視窗")
+    message.pack()#放進去
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
