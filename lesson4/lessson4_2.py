3f019ac7262695d7701f75286c6a65cebe90e3e4
13 <st27788711@gmail.com>
2024-10-18 13:36:06 +0800

    lesson4_2_5

--- lesson4/lessson4_2.py
+++ lesson4/lessson4_2.py
@@ -3,16 +3,15 @@ import tkinter as tk
 class window(tk.Tk):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
+        self.title('這是我的第一個視窗')
+        self.geometry('800x300')
+        message = tk.Label(self,text="Hollo!這是我的第一個視窗")
+        message.pack()#放進去
 
 
 def main():
-    root = window()
-    print(type(root))
-    root.title('這是我的第一個視窗')
-    root.geometry('800x300')
-    message = tk.Label(root,text="Hollo!這是我的第一個視窗")
-    message.pack()#放進去
-    root.mainloop()
+    window1 = window()
+    window1.mainloop()
 
 
 if __name__ == '__main__':
