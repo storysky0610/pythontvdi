989e8fed852bf282b45375bf7b9aea38326cc30a
13 <st27788711@gmail.com>
2024-10-18 16:40:24 +0800

    ADD

--- lesson4/lesson4_4.py
+++ lesson4/lesson4_4.py
@@ -3,9 +3,9 @@ from tkinter import ttk
 class Window(tk.Tk):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
-        self.title('使用ttk的套件')
+        self.title('使用ttk的套件')#全域變數
         self.geometry('400x300')
-        message = ttk.Label(self,text="使用ttk的Label")
+        message = ttk.Label(self,text="使用ttk的Label")#區域變數
         message.pack()
 def main():
     window = Window()
