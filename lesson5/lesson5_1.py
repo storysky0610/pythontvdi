887bb077e8a389c2eb619f0e4018215a02540f68
13 <st27788711@gmail.com>
2024-10-21 11:48:33 +0800

    lesson5_1_0

---
+++ lesson5/lesson5_1.py
@@ -0,0 +1,13 @@
+from tkinter import ttk
+from ttkthemes import ThemedTk
+
+class Window (ThemedTk):
+    def __init__(self,*args,**kwargs):
+        super().__init__(self,*args,**kwargs)
+        
+def main():
+    window = Window(theme="breeze")
+    window.mainloop()
+    
+if __name__ == '__main__':
+    main()
