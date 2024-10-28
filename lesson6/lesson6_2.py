5deb202fa26c99c64531d8410fc790601e232a1e
13 <st27788711@gmail.com>
2024-10-28 14:46:01 +0800

    add

--- lesson6/lesson6_2.py
+++ lesson6/lesson6_2.py
@@ -28,18 +28,48 @@ class Window(ThemedTk):
         self.selected_site = tk.StringVar()
         sitename_cb = ttk.Combobox(bottomFrame,textvariable=self.selected_site,state='readonly',values=sitename)
         # sitename_cb.configurestate='readonly')
-        self.selected_site.set('請選擇站點')
-        sitename_cb.pack()
+        self.selected_site.set('請選擇站點')        
+        sitename_cb.bind('<<ComboboxSelected>>', self.sitename_selected)
+        sitename_cb.pack(side='left',expand=True,anchor='n')
 
+        columns = ('data','county','aqi','pm25','status','lat','lon')
+
+        tree = ttk.Treeview(bottomFrame, columns=columns, show='headings')
+
+        # define headings
+        tree.heading('data', text='日期')
+        tree.heading('county', text='縣市')
+        tree.heading('aqi', text='AQI')
+        tree.heading('pm25', text='PM25')
+        tree.heading('status', text='status')
+        tree.heading('lat', text='緯度')
+        tree.heading('lon', text='經度')
+
+        tree.column('data', width=150,anchor='center')
+        tree.column('county', width=80,anchor='center')
+        tree.column('aqi', width=50,anchor='center')
+        tree.column('pm25', width=50,anchor='center')
+        tree.column('status', width=50,anchor='center')
+        tree.column('lat', width=100,anchor='center')
+        tree.column('lon', width=100,anchor='center')
+
+        tree.insert("",'end',values=('2024-10-28 9:00','屏東',17,6.5,'良好',22.260899,120.651472))
+
+        # # generate sample data
+        # contacts = []
+        # for n in range(1, 100):
+        #     contacts.append((f'first {n}', f'last {n}', f'email{n}@example.com'))
+
+        # # add data to the treeview
+        # for contact in contacts:
+        #     tree.insert('', tk.END, values=contact)
+        tree.pack(side='right')
+        #,expand=True,anchor='n'
         bottomFrame.pack(expand=True,fill='x',padx=20,pady=(0,20),ipadx=10,ipady=10)
         #==============end bottomFrame===============
     
-    def agreement_changed(self):
-        showinfo(
-            title='Agreement',
-            message= self.agreement.get()
-
-        )
+    def sitename_selected(self,event):
+        print(self.selected_site.get())
     
     
         
