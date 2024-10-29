90f01e4d435b19baf06f0f46e4b8cd20498ad1e9
13 <st27788711@gmail.com>
2024-10-29 08:26:30 +0800

    add

--- lesson6/datasource.py
+++ lesson6/datasource.py
@@ -9,7 +9,23 @@ def get_sitenames()->list[str]:
         print(e)
     else:
         sitenames = set()
-        for item in data['records']:
-            sitenames.add(item['sitename'])
+        for items in data['records']:
+            sitenames.add(items['sitename'])
         sitenames = list(sitenames)
         return sitenames
+def get_seiected_data(sitename:str)->list[list]:
+    url = 'https://data.moenv.gov.tw/api/v2/aqx_p_488?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON'
+    try:
+        response = requests.get(url)
+        response .raise_for_status()
+        data = response.json()
+    except Exception as e:
+        print(e)
+    else:
+        outerlist= []
+        for items in data['records']:
+            if items['sitename'] == sitename:
+                innerlist=[items['datacreationdata'],items['county'],items['aqi'],items['pm2.5'],items['status'],items['latitude'],items['longitude']]
+
+                outerlist.append(innerlist)
+        return outerlist
--- lesson6/lesson6_2.py
+++ lesson6/lesson6_2.py
@@ -34,26 +34,24 @@ class Window(ThemedTk):
 
         columns = ('data','county','aqi','pm25','status','lat','lon')
 
-        tree = ttk.Treeview(bottomFrame, columns=columns, show='headings')
+        self.tree = ttk.Treeview(bottomFrame, columns=columns, show='headings')
 
         # define headings
-        tree.heading('data', text='日期')
-        tree.heading('county', text='縣市')
-        tree.heading('aqi', text='AQI')
-        tree.heading('pm25', text='PM25')
-        tree.heading('status', text='status')
-        tree.heading('lat', text='緯度')
-        tree.heading('lon', text='經度')
-
-        tree.column('data', width=150,anchor='center')
-        tree.column('county', width=80,anchor='center')
-        tree.column('aqi', width=50,anchor='center')
-        tree.column('pm25', width=50,anchor='center')
-        tree.column('status', width=50,anchor='center')
-        tree.column('lat', width=100,anchor='center')
-        tree.column('lon', width=100,anchor='center')
-
-        tree.insert("",'end',values=('2024-10-28 9:00','屏東',17,6.5,'良好',22.260899,120.651472))
+        self.tree.heading('data', text='日期')
+        self.tree.heading('county', text='縣市')
+        self.tree.heading('aqi', text='AQI')
+        self.tree.heading('pm25', text='PM25')
+        self.tree.heading('status', text='status')
+        self.tree.heading('lat', text='緯度')
+        self.tree.heading('lon', text='經度')
+        self.tree.column('data', width=150,anchor='center')
+        self.tree.column('county', width=80,anchor='center')
+        self.tree.column('aqi', width=50,anchor='center')
+        self.tree.column('pm25', width=50,anchor='center')
+        self.tree.column('status', width=50,anchor='center')
+        self.tree.column('lat', width=100,anchor='center')
+        self.tree.column('lon', width=100,anchor='center')
+        self.tree.insert("",'end',values=('2024-10-28 9:00','屏東',17,6.5,'良好',22.260899,120.651472))
 
         # # generate sample data
         # contacts = []
@@ -63,13 +61,17 @@ class Window(ThemedTk):
         # # add data to the treeview
         # for contact in contacts:
         #     tree.insert('', tk.END, values=contact)
-        tree.pack(side='right')
+        self.tree.pack(side='right')
         #,expand=True,anchor='n'
         bottomFrame.pack(expand=True,fill='x',padx=20,pady=(0,20),ipadx=10,ipady=10)
         #==============end bottomFrame===============
     
     def sitename_selected(self,event):
-        print(self.selected_site.get())
+        selected = self.selected_site.get()
+        selected_data = datasource.get_selected_data(selected)
+        for record in selected_data:
+            self.tree.insert('','end',values=record)
+        
     
     
         
