d50148ad64a72bd6c9496a36338e7a42d5b8fd80
13 <st27788711@gmail.com>
2024-11-04 16:44:55 +0800

    ADD

---
+++ lesson7/datasource.py
@@ -0,0 +1,77 @@
+import requests
+import sqlite3
+def get_sitename()->list[str]:
+    '''
+    docString
+    parameter:
+    return:
+        傳出所有的站點名稱
+    '''
+    conn = sqlite3.connect("lesson7/AQI.db")
+    with conn:
+        # Create a cursor object to execute SQL commands
+        cursor = conn.cursor()
+        # SQL query to select unique sitenames from records table
+        sql = '''
+        SELECT DISTINCT sitename
+        FROM records
+        '''
+        # Execute the SQL query
+        cursor.execute(sql)
+        # Get all results and extract first item from each row into a list
+        sitenames = [items[0] for items in cursor.fetchall()]
+    
+    # Return the list of unique sitenames
+    return sitenames
+    
+def get_selected_data(sitename:str)->list[list]:
+    '''
+    使用者選擇了sitename,並將sitename傳入
+    Parameter:
+        sitename: 站點的名稱
+    Return:
+        所有關於此站點的相關資料
+    '''
+    url = 'https://data.moenv.gov.tw/api/v2/aqx_p_488?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=JSON'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+    except Exception as e:
+        print(e)
+    else:
+        outerlist = []
+        for items in data['records']:
+            if items['sitename'] == sitename:
+                innerlist = [items['datacreationdate'],items['county'],items['aqi'],items['pm2.5'],items['status'],items['latitude'],items['longitude']]
+                outerlist.append(innerlist)
+
+            
+        return outerlist
+    
+def download_data():
+    conn = sqlite3.connect("lesson7/AQI.db")
+    url = 'https://data.moenv.gov.tw/api/v2/aqx_p_488?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=JSON'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+    except Exception as e:
+        print(e)
+    else:
+        sitenames = set()
+        with conn:
+            cursor = conn.cursor()
+            for items in data['records']:
+                sitename = items['sitename']
+                county = items['county']
+                aqi = int(items['aqi']) if items['aqi'] != '' else 0
+                status = items['status']
+                pm25 = float(items['pm2.5']) if items['pm2.5'] != '' else 0.0
+                date = items['datacreationdate']
+                lon = float(items['longitude']) if items['longitude'] != '' else 0.0
+                lat = float(items['latitude']) if items['latitude'] != '' else 0.0
+                sql = '''INSERT OR IGNORE INTO records(sitename,county,aqi,status,pm25,date,lat,lon)
+                        values (?,?, ?, ?,?,?,?,?);
+                '''
+                cursor.execute(sql,(sitename, county, aqi, status,pm25,date,lon,lat))
