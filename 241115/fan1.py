76801b29442be035ed9dfecf07c3ebceef01bc90
13 <st27788711@gmail.com>
2024-11-19 09:09:36 +0800

    ADD

--- 241115/fan1.py
+++ 241115/fan1.py
@@ -4,7 +4,7 @@ import pandas as pd
 import csv
 
 # 读取CSV文件，获取Plant、Workstation Code和Code数据
-csv_file =  r'C:\Users\USER\GitHub\pythontvdi\241115\view\Factoryworkstation.csv' # 请替换为你的实际路径
+csv_file =  r'C:\Users\user\Documents\GitHub\tivd\pythontvdi\241115\Factoryworkstation.csv' # 请替换为你的实际路径
 df = pd.read_csv(csv_file)
 # print(df)  # 打印数据框以检查内容
 # 提取Plant和Workstation Code列，去除重复值
