e19c7e1acb6a07f139e8eeb6aff1cfab396e52dc
13 <st27788711@gmail.com>
2024-11-22 07:25:11 +0800

    add

--- 241115/風扇良率判定資訊查詢系統/fan1.py
+++ 241115/風扇良率判定資訊查詢系統/fan1.py
@@ -4,14 +4,24 @@ import pandas as pd
 import csv
 
 # 读取CSV文件，获取Plant、Workstation Code和Code数据
-csv_file =  r'C:\Users\user\Documents\GitHub\tivd\pythontvdi\241115\Factoryworkstation.csv' # 请替换为你的实际路径
-df = pd.read_csv(csv_file)
+ # 请替换为你的实际路径
+df = pd.read_csv('Factoryworkstation.csv')
 # print(df)  # 打印数据框以检查内容
 # 提取Plant和Workstation Code列，去除重复值
 plant_list = df['Plant'].dropna().unique().tolist()
 Code_list = df['Code'].dropna().unique().tolist()
 WorkstationCode_list = df['Workstation Code'].dropna().unique().tolist()
 
-print(plant_list)
-print(WorkstationCode_list)
-print(Code_list)
+# print(plant_list)
+# print(WorkstationCode_list)
+# print(Code_list)
+
+    # 讀取CSV檔案
+data = pd.read_csv('virtual_data_with_permissions.csv')
+
+    # 根據權限代號過濾各角色
+roles = {
+        "製造者": data[data['權限代號'] == 1].set_index('ID')['姓名'].to_dict(),
+        "組長": data[data['權限代號'] == 2].set_index('ID')['姓名'].to_dict(),
+        "品保員": data[data['權限代號'] == 3].set_index('ID')['姓名'].to_dict(),
+    }
